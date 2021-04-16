49190ea127e37eb667833242e8f9cd75020c4fdc
EMMAD Developer <emmadsadiq55@gmail.com>
2021-04-17 03:12:56 +0500

    add images

---
+++ js/navbar.js
@@ -0,0 +1,22 @@
+document.getElementById("drop-1").addEventListener("click", ()=> {
+  let element = document.getElementById("menu-1");
+    element.classList.toggle("show-menu");
+   document.getElementById("drop-1-icon").classList.toggle("rotate_icon")
+});
+document.getElementById("drop-2").addEventListener("click", ()=> {
+  let element = document.getElementById("menu-2");
+  element.classList.toggle("show-menu");
+  document.getElementById("drop-2-icon").classList.toggle("rotate_icon")
+
+});
+document.getElementById("togle-menu").addEventListener("click", (e)=> {
+  let element = document.getElementById("mobile-menu");
+  element.classList.toggle("show-menu");
+  let icon = document.getElementById("togle-menu")
+  if(icon.className!="fas fa-bars"){
+    icon.className="fas fa-bars"
+  }else{
+    icon.className="fas fa-times"
+
+  }
+});
